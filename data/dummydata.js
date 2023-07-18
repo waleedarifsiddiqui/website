5c90e31e58582fb2884cfcdd1a50f95f5ee5d4a8
waleedarifsiddiqui <118274880+waleedarifsiddiqui@users.noreply.github.com>
2023-07-18 03:51:18 -0700

    Add files via upload

---
+++ data/dummydata.js
@@ -0,0 +1,252 @@
+import { AiOutlineCalendar } from "react-icons/ai";
+import { TfiRulerPencil } from "react-icons/tfi";
+import { VscFileSubmodule } from "react-icons/vsc";
+import { BiUser } from "react-icons/bi";
+
+export const home = [
+  {
+    icon: <AiOutlineCalendar size={25} />,
+    title: "10+ years of market experience",
+  },
+  {
+    icon: <TfiRulerPencil size={25} />,
+    title: "Unique technologies & modern approach ",
+  },
+  {
+    icon: <VscFileSubmodule size={25} />,
+    title: "100+ cuccessful cases in portfolio",
+  },
+  {
+    icon: <BiUser size={25} />,
+    title: "Customer satisfaction is our top priority",
+  },
+];
+export const expertise = [
+  {
+    id: 1,
+    title: "Custom Web Development",
+    cover: "../images/e1.jpg",
+    desc: [
+      { text: "Web design" },
+      { text: "Web development" },
+      { text: "Custom icons & illustrations" },
+      { text: "Hosting" },
+      { text: "Website audit" },
+    ],
+    para1:
+      "We take great pride in delivering customized web development services tailored to meet the unique needs of our clients. Our team is composed of highly skilled developers who use the latest technology and best practices to transform your vision into a reality. It entails creating an intuitive, secure, and functional website utilizing many technologies and programming languages such as HTML, CSS, JavaScript and React.",
+    para2:
+      "From designing captivating interfaces that reflect brand identity to integrating robust functionalities into every corner of a website's architecture; they leave no stone unturned in providing impeccable end-to-end solutions that resonate with client objectives – rendering null room for dissatisfaction or compromise.",
+      para3: "We understand that each organization has distinct requirements, so we create websites that not only reflect your identity, but also deliver seamless functionality and an exceptional user experience. We are committed to providing rigorous planning, flawless execution, and continuous communication with our clients, ensuring that each project receives our undivided attention from start to finish.",
+      para4: "By utilizing cutting-edge tools and frameworks, we guarantee the highest level of performance, security, and scalability for your website - empowering your online presence and making a lasting impact in today's digital landscape.",
+      para5: "For more information on how our bespoke web development services may elevate your online presence, get in touch with us right away.",
+      para6: "We understand that each organization has distinct requirements, so we create websites that not only reflect your identity, but also deliver seamless functionality and an exceptional user experience. We are committed to providing rigorous planning, flawless execution, and continuous communication with our clients, ensuring that each project receives our undivided attention from start to finish. By utilizing cutting-edge tools and frameworks, we guarantee the highest level of performance, security, and scalability for your website - empowering your online presence and making a lasting impact in today's digital landscape.",
+      para7: "We understand that each organization has distinct requirements, so we create websites that not only reflect your identity, but also deliver seamless functionality and an exceptional user experience. We are committed to providing rigorous planning, flawless execution, and continuous communication with our clients, ensuring that each project receives our undivided attention from start to finish. By utilizing cutting-edge tools and frameworks, we guarantee the highest level of performance, security, and scalability for your website - empowering your online presence and making a lasting impact in today's digital landscape.",
+  },
+  {
+    id: 2,
+    title: "WordPress Websites",
+    cover: "../images/e2.jpg",
+    desc: [
+      { text: "Social media marketing" },
+      { text: "Marketing campaigns" },
+      { text: "Markting management" },
+      { text: "SEO" },
+    ],
+  },
+  {
+    id: 3,
+    title: "E-Commerce",
+    cover: "../images/e3.jpg",
+    desc: [
+      { text: "E-Commerce website design" },
+      { text: "WooCommerce" },
+      { text: "Content management" },
+      { text: "Hosting" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Branding & Creative Services",
+    cover: "../images/e4.jpg",
+    desc: [
+      { text: "Visual identity" },
+      { text: "Branding for social media" },
+      { text: "Custom illustrations" },
+    ],
+  },
+];
+export const testimonial = [
+  {
+    id: 1,
+    name: "Alexander Black",
+    cover: "../images/e1.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 2,
+    name: "Diana Green",
+    cover: "../images/e2.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+  {
+    id: 3,
+    name: "Alexander Black",
+    cover: "../images/e3.jpg",
+    post: "Seven consulting - CEO",
+    desc: "Nunc fermentum - tempus erat ligula, sit amet lacinia justo cursus ac. Suspendisse quis nulla tincidunt! Lorem ipsum dolor amet at ornare ex, quis fringilla tortor! Nunc consectetur feugiat rutrum. Sed rhoncus sapien!",
+  },
+  {
+    id: 4,
+    name: "Diana Green",
+    cover: "../images/e4.jpg",
+    post: "Seven Arts - marketing manager",
+    desc: "Cras at ornare fermentum quam et tortor euismod, vel maximus metus tristique at ornare ex, quis fringilla tortor. Aenean semper neque quis consectetur lobortis. Quisque nec convallis ex. Aenean ut metus et nunc cursus aliquet.",
+  },
+];
+export const showcase = [
+  {
+    id: 1,
+    title: "Neon Lights",
+    cover: "../images/s1.jpg",
+    catgeory: "DIGITAL MARKETING",
+  },
+  {
+    id: 2,
+    title: "Futuristic Furniture",
+    catgeory: "WEBSITES",
+    cover: "../images/s2.jpg",
+  },
+  {
+    id: 3,
+    title: "Smart Living",
+    cover: "../images/s3.jpg",
+    catgeory: "WEBSITES",
+  },
+  {
+    id: 4,
+    title: "Light Painting",
+    cover: "../images/s4.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "Ideabox",
+    cover: "../images/s5.jpg",
+    catgeory: "BRANDING	",
+  },
+  {
+    id: 5,
+    title: "VR Experience",
+    cover: "../images/s6.jpg",
+    catgeory: "WEBSITES	",
+  },
+];
+export const brand = [
+  {
+    id: 1,
+    cover: "../images/l1.svg",
+  },
+  {
+    id: 2,
+    cover: "../images/l2.svg",
+  },
+  {
+    id: 3,
+    cover: "../images/l3.svg",
+  },
+  {
+    id: 4,
+    cover: "../images/l4.svg",
+  },
+  {
+    id: 5,
+    cover: "../images/l5.svg",
+  },
+  {
+    id: 6,
+    cover: "../images/l6.svg",
+  },
+];
+export const blogdata = [
+  {
+    id: 1,
+    title: "Custom Web Development",
+    cover: "../images/b1.webp",
+    catgeory: "",
+    date: "",
+  },
+  {
+    id: 2,
+    title: "WordPress Websites",
+    cover: "../images/b2.jpg",
+    catgeory: "",
+    date: "",
+  },
+  {
+    id: 3,
+    title: "E-Commerce",
+    cover: "../images/b3.jpg",
+    catgeory: "",
+    date: "",
+  },
+  {
+    id: 4,
+    title: "Branding & Creative Services",
+    cover: "../images/b4.jpg",
+    catgeory: "",
+    date: "",
+  },
+];
+export const teamdata = [
+  {
+    id: 1,
+    title: "Alexander Black",
+    cover: "../images/t1.jpg",
+    post: "FOUNDER, CEO",
+  },
+  {
+    id: 2,
+    title: "Anna Kovalenko",
+    cover: "../images/t2.jpg",
+    post: "FINANCE DIRECTOR",
+  },
+  {
+    id: 3,
+    title: "Tiffany White",
+    cover: "../images/t3.jpg",
+    post: "CREATIVE DIRECTOR",
+  },
+  {
+    id: 4,
+    title: "Richard Greenwood",
+    cover: "../images/t4.jpg",
+    post: "PROGRAMMER",
+  },
+  {
+    id: 5,
+    title: "Jessica Brown",
+    cover: "../images/t5.jpg",
+    post: "MARKETING DIRECTOR",
+  },
+  {
+    id: 6,
+    title: "Gregory Windstorm",
+    cover: "../images/t6.jpg",
+    post: "ACCOUNTING MANAGER",
+  },
+  {
+    id: 7,
+    title: "Anna Red",
+    cover: "../images/t7.jpg",
+    post: "PROJECT MANAGER",
+  },
+  {
+    id: 8,
+    title: "Join our team!",
+    cover: "../images/t8.jpg",
+    post: "",
+  },
+];

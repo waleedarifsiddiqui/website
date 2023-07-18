93379a7580dd9a1494b185c6f1bc5af107e8d180
Waleed <waleedarifsiddiqui@gmail.com>
2023-07-18 13:37:28 +0500

    Master

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Hero } from "@/sections"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>R-TechQA Design</title>
+      </Head>
+      <Hero />
+    </>
+  )
+}
